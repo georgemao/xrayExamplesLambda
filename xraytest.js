2148840d536e7c5418a05612aff0414711c33422
georgmao <georgmao@amazon.com>
2017-10-13 13:31:14 -0700

    first commit

---
+++ xraytest.js
@@ -0,0 +1,65 @@
+'use strict'
+//process.env['AWS_XRAY_DAEMON_ADDRESS'] = '127.0.0.1:3000';
+
+var AWSXRay = require('aws-xray-sdk-core');
+var AWS  = AWSXRay.captureAWS(require('aws-sdk'));
+AWS.config.update({region:'us-west-2'});
+var s3 = new AWS.S3();
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+// This is a workaround that is needed for running in SAM Local because it does not generate the _X_AMZN_TRACE_ID
+// See: http://docs.aws.amazon.com/lambda/latest/dg/lambda-x-ray.html#lambda-x-ray-env-variables
+var segment = new AWSXRay.Segment("999999", "1-59bc04e6-7e2f175630c09bcb8fb154f6");
+var ns = AWSXRay.getNamespace();
+AWSXRay.setSegment(segment);
+
+exports.handler = function (event, context, callback) {   
+    var segment = AWSXRay.getSegment();
+     
+    console.log(JSON.stringify(segment));
+
+    console.log(process.env.X_AMZN_TRACE_ID);
+    console.log(process.env.TESTVAR);
+    
+    params ={
+        Bucket: 'doesnotexistblsucket',
+        Key:'key',
+        Body:'mybody'
+    };
+
+    s3.putObject(params, function(err, data){
+
+    });
+
+
+
+    var params ={
+        Bucket: 'awsgeorge',
+        Key:'key',
+        Body:'mybody'
+    };
+    
+    s3.putObject(params, function(err, data){
+        
+    });
+
+    
+
+    params = {
+        TableName:'Buckets'
+    };
+    
+    docClient.scan(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        data.Items.forEach(function(element, index, array) {
+          console.log(element);
+        });
+      }
+    });
+
+    
+
+    callback(null, {"body" : "testbody"} )
+}
