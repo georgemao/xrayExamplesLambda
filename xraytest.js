1bf7dd796e8c13c40653fc2d08e2076bda8631ac
georgmao <georgmao@amazon.com>
2018-03-02 15:09:44 -0500

    Add annotations examples

--- xraytest.js
+++ xraytest.js
@@ -2,53 +2,53 @@
 // Xray is enabled and tracing all calls. We can use Xray to identify the errors very effectively
 
 'use strict'
-//process.env['AWS_XRAY_DAEMON_ADDRESS'] = '127.0.0.1:3000';
-
 var AWSXRay = require('aws-xray-sdk-core');
 var AWS  = AWSXRay.captureAWS(require('aws-sdk'));
-AWS.config.update({region:'us-west-2'});
 var s3 = new AWS.S3();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-// This is a workaround that is needed for running in SAM Local because it does not generate the _X_AMZN_TRACE_ID
-// See: http://docs.aws.amazon.com/lambda/latest/dg/lambda-x-ray.html#lambda-x-ray-env-variables
-var segment = new AWSXRay.Segment("999999", "1-59bc04e6-7e2f175630c09bcb8fb154f6");
-var ns = AWSXRay.getNamespace();
-AWSXRay.setSegment(segment);
 
-exports.handler = function (event, context, callback) {   
-    var segment = AWSXRay.getSegment();
-     
-    console.log(JSON.stringify(segment));
-
-    console.log(process.env.X_AMZN_TRACE_ID);
-    console.log(process.env.TESTVAR);
+exports.handler = function (event, context, callback) {
+    //console.log("ENV: "+process.env.AWS_XRAY_DAEMON_ADDRESS);
     
     // Try to upload to a bucket that does not exist - will Fail
-    params ={
+    AWSXRay.captureAsyncFunc('Put to doesnotexistbucket', function(subsegment) {
+      var params ={
         Bucket: 'doesnotexistbucket',
         Key:'key',
         Body:'mybody'
-    };
+      };
 
-    s3.putObject(params, function(err, data){
-
-    });
-
-    // Upload to a bucket that exists - IAM role does not provide permissions to
-    var params ={
-        Bucket: 'awsgeorge',
-        Key:'key',
-        Body:'mybody'
-    };
-    
-    s3.putObject(params, function(err, data){
+      s3.putObject(params, function(err, data){
+        if(err){
+          subsegment.addAnnotation('error', 'error');
+          subsegment.addMetadata('error', err.stack);
+        }
         
+        subsegment.close();
+      });
+      
     });
-
+    
+    // Upload to a bucket that exists - IAM role does not provide permissions to -- will fail
+    AWSXRay.captureAsyncFunc('Put to awsgeorge', function(subsegment) {
+      var params ={
+          Bucket: 'awsgeorge',
+          Key:'key',
+          Body:'mybody'
+      };
+      
+      s3.putObject(params, function(err, data){
+        if(err){
+          subsegment.addAnnotation('error', 'error');
+          subsegment.addMetadata('error', err.stack);
+        }
+        subsegment.close();
+      });
+    });
     
     // Perform a DDB Scan - will succeed
-    params = {
+    var params = {
         TableName:'Buckets'
     };
     
@@ -61,8 +61,6 @@ exports.handler = function (event, context, callback) {
         });
       }
     });
-
     
-
-    callback(null, {"body" : "testbody"} )
+    callback(null, {"body" : "success!!!"} )
 }
